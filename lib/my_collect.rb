bd52454d8385a1317145242d3d460b425490bf5b
Elijah wines <elijahwines84@yahoo.com>
2020-02-19 14:07:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def give_first_name(array)
+  i = 0
+  full_names = []
+  while i < array.length
+    full_names << yield(array[i])
+    i += 1 
+  end
+  full_names
+end
 
+give_first_name(["Elijah Wines", "Taylor O'Neal", "Carly Dixey", "Aaron Dixey", "Madeline Wines", "Charlotte Dixey"]) do |name|
+  name.split(" ").first
+end
 
